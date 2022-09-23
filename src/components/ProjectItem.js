71883126b6657809789a288c749e8220cb79fc05
+ <+>
2022-09-23 09:14:36 +0300

    Task Completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
-  const technologiesList = technologies.map((tech) => (
-    <span key={tech}>{tech}</span>
-  ));
+function ProjectItem({ id, name, about, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">{technologiesList}</div>
+      <div className="technologies">
+        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => {
+          return (
+            <span key={technology}>{technology}</span>
+          )
+        })}
+      </div>
     </div>
   );
 }
